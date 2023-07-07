bc3de41bc40d2843a2b2a38a410796591a0c9418
Kenneth Ofosu <kennethofosu01@gmail.com>
2023-07-07 18:45:03 -0400

    update App.tsx with updated selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useRef, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { drawStroke, clearCanvas, setCanvasSize } from "./utils/canvasUtils"
-import { beginStroke, endStroke, updateStroke } from "./actions"
-import {
-  currentStrokeSelector,
-  historyIndexSelector,
-  strokesSelector
-} from "./rootReducer"
+import { beginStroke, endStroke, updateStroke } from "./modules/currentStroke/actions"
+import { strokesSelector } from "./modules/strokes/reducer"
+import { currentStrokeSelector } from "./modules/currentStroke/reducer"
+import { historyIndexSelector } from "./modules/historyIndex/reducer"
 import { EditPanel } from "./shared/EditPanel"
 import { ColorPanel } from "./shared/ColorPanel"
 
@@ -75,7 +73,7 @@ function App() {
 
   const endDrawing = () => {
     if (isDrawing) {
-      dispatch(endStroke())
+      dispatch(endStroke(historyIndex, currentStroke))
     }
   }
 
