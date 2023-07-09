474f0d9261b81b43a8642bb35b90ec4fb45801f0
Kenneth Ofosu <kennethofosu01@gmail.com>
2023-07-09 13:22:07 -0400

    update currentStroke reducer with createReducer

--- src/modules/currentStroke/reducer.ts
+++ src/modules/currentStroke/reducer.ts
@@ -1,41 +1,26 @@
-import { Action, updateStroke, beginStroke, setStrokeColor } from "./actions";
+import { updateStroke, beginStroke, setStrokeColor } from "./actions";
 import { endStroke } from "../sharedActions";
 import { RootState } from "../../utils/types";
+import { createReducer } from "@reduxjs/toolkit";
 
 const initialState: RootState["currentStroke"] = {
   points: [],
   color: "#000",
 };
 
-export const reducer = (
-  state: RootState["currentStroke"] = initialState,
-  action: Action
-) => {
-  switch (action.type) {
-    case beginStroke.toString(): {
-      return { ...state, points: [action.payload] };
-    }
-    case updateStroke.toString(): {
-      return {
-        ...state,
-        points: [...state.points, action.payload],
-      };
-    }
-    case setStrokeColor.toString(): {
-      return {
-        ...state,
-        color: action.payload,
-      };
-    }
-    case endStroke.toString(): {
-      return {
-        ...state,
-        points: [],
-      };
-    }
-    default:
-      return state;
-  }
-};
+export const reducer = createReducer(initialState, (builder) => {
+  builder.addCase(beginStroke, (state, action) => {
+    state.points = [action.payload];
+  });
+  builder.addCase(updateStroke, (state, action) => {
+    state.points.push(action.payload);
+  });
+  builder.addCase(setStrokeColor, (state, action) => {
+    state.color = action.payload;
+  });
+  builder.addCase(endStroke, (state) => {
+    state.points = [];
+  });
+});
 
 export const currentStrokeSelector = (state: RootState) => state.currentStroke;
